b4126f2dd2725f3a6d097444492663d2dd4c3067
Misha Gorodnitzky <misaka@pobox.com>
2017-12-01 23:53:37 +0000

    Making bob.exs work

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,6 +1,12 @@
 defmodule Bob do
   def hey(input) do
     cond do
+      String.match?(input, ~r/^\s*$/) -> "Fine. Be that way!"
+      String.last(input) == "?" -> "Sure."
+      String.replace(input, ~r/\P{L}/, "") |>
+          String.match?(~r/^\p{Lu}+$/) ->
+          "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end

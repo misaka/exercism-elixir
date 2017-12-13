a9ced7414ed4b41d2c1a9ddae3a862d6dfca1e0c
Misha Gorodnitzky <misaka@pobox.com>
2017-12-13 18:13:19 +0000

    Make rotational-cipher work.

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -7,7 +7,29 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
-  def rotate(text, shift) do
+  def rotate(text, shift) when not is_list(text) do
+    to_string(rotate(to_charlist(text), shift))
+  end
+
+  def rotate([head | tail], shift) when 97 <= head and head <= 122 do
+    [rotate_character(head, 97, shift) | rotate(tail, shift)]
+  end
+
+  def rotate([head | tail], shift) when 65 <= head and head <= 90 do
+    [rotate_character(head, 65, shift) | rotate(tail, shift)]
+  end
+
+  def rotate([head | tail], shift) do
+    [head | rotate(tail, shift)]
+  end
+
+  def rotate([], shift) do
+    []
+  end
+
+  def rotate_character(character, offset, shift) do
+    letter = character - offset
+    offset + Integer.mod((letter + shift), 26)
   end
 end
 

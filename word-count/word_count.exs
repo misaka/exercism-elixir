06b43ef3795a74c7033f81b81d17f890d56fa43f
Misha Gorodnitzky <misaka@pobox.com>
2018-01-23 18:18:16 +0000

    solution for word count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,20 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    sentence |> String.split(~r{[^\pL\pN-]}u) |> count_words(%{})
+  end
 
+  def count_words(["" | tail], counts) do
+    count_words(tail, counts)
+  end
+
+  def count_words([word | tail], counts) do
+    word_lower = word |> String.downcase
+    {_prev_count, new_counts} = get_and_update_in(counts, [word_lower], &{&1, (&1 || 0) + 1})
+    count_words(tail, new_counts)
+  end
+
+  def count_words([], counts) do
+    counts
   end
 end

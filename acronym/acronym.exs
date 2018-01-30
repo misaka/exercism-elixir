62d000a1356d978fcd319b1e267194ddf2e36345
Misha Gorodnitzky <misaka@pobox.com>
2018-01-30 09:09:35 +0000

    Solution for acronym

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -4,6 +4,13 @@ defmodule Acronym do
   "This is a string" => "TIAS"
   """
   @spec abbreviate(String.t()) :: String.t()
-  def abbreviate(string) do
+  def abbreviate(string) when is_bitstring(string) do
+    abbreviate(string |> String.split( ~r/\P{L}+|\p{Ll}(?=\p{Lu})/ )) |> List.to_string
+  end
+  def abbreviate([word | tail]) do
+    [word |> String.at(0) |> String.upcase | abbreviate(tail)]
+  end
+  def abbreviate([]) do
+    ""
   end
 end

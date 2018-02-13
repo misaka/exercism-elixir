78152dcafc28566fd13002ebabe1f25ff099d64f
Misha Gorodnitzky <misaka@pobox.com>
2018-02-13 16:58:27 +0000

    solution for run-length-encoding

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -8,11 +8,54 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t) :: String.t
   def encode(string) do
+    encode(string, '')
+  end
 
+  def encode("", '') do
+    ""
+  end
+
+  def encode(<<char>> <> string, '') do
+    encode(string, [char])
+  end
+
+  def encode(<<char>> <> string, [char | enc_tail]) do
+    encode(string, [char | [char | enc_tail]])
+  end
+
+  def encode(string, enc_chars = [char | _]) when length(enc_chars) > 1 do
+    (enc_chars |> length |> to_string) <> <<char>> <> encode(string, '')
+  end
+
+  def encode(string, enc_chars) do
+    <<(enc_chars |> Enum.at(0))>> <> encode(string, '')
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
+    decode(string, "")
+  end
 
+  def decode(<<char>> <> string, count) when char in '0123456789' do
+    decode(string, count <> <<char>>)
+  end
+
+  def decode(<<char>> <> string, "") do
+    <<char>> <> decode(string, "")
+  end
+
+  def decode(<<char>> <> string, count) do
+    repeat(char, count |> String.to_integer) <> decode(string, "")
+  end
+
+  def decode("", "") do
+    ""
+  end
+
+  def repeat(_char, 0) do
+    ""
+  end
+  def repeat(char, count) do
+    <<char>> <> repeat(char, count-1)
   end
 end

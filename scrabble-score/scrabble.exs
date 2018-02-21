42caf9c89c13b9a5342d38235293fafd6691c652
Misha Gorodnitzky <misaka@pobox.com>
2018-02-21 17:55:31 +0000

    Solution for scrabble score

--- scrabble-score/scrabble.exs
+++ scrabble-score/scrabble.exs
@@ -3,7 +3,20 @@ defmodule Scrabble do
   Calculate the scrabble score for the word.
   """
   @spec score(String.t) :: non_neg_integer
-  def score(word) do
-
+  def score(<<letter>> <> word) do
+    <<letter>> = <<letter>> |> String.downcase
+    letter_score(letter) + score(word)
   end
+  def score("") do 0 end
+
+
+  def letter_score(letter) when letter in 'aeioulnrst' do 1 end
+  def letter_score(letter) when letter in 'dg'         do 2 end
+  def letter_score(letter) when letter in 'bcmp'       do 3 end
+  def letter_score(letter) when letter in 'fhvwy'      do 4 end
+  def letter_score(letter) when letter in 'k'          do 5 end
+  def letter_score(letter) when letter in 'jx'         do 8 end
+  def letter_score(letter) when letter in 'qz'         do 10 end
+  def letter_score(_)                                  do 0 end
+
 end
